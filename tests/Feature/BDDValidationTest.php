b23677babd8b0ec5c18af25c11f836b2addd1f8a
sophie <faugeras.info@gmail.com>
2025-12-06 17:22:45 +0100

    Intégration des tests 5

--- tests/Feature/BDDValidationTest.php
+++ tests/Feature/BDDValidationTest.php
@@ -9,9 +9,9 @@ class BDDValidationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_voir_la_connexion_de_test()
-    {
-        $this->assertSame('mysql', config('database.default'));
-        $this->assertSame('todo_test', config('database.connections.mysql.database'));
-    }
+    // public function test_voir_la_connexion_de_test()
+    // {
+    //     $this->assertSame('mysql', config('database.default'));
+    //     $this->assertSame('todo_test', config('database.connections.mysql.database'));
+    // }
 }
