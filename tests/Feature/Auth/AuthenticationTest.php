f95ebea4cd3857c32a8334466f2f4ffccd8bfcda
sophie <faugeras.info@gmail.com>
2025-12-09 08:25:09 +0100

    Intégration des tests 14

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -10,12 +10,21 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
+    // public function test_login_screen_can_be_rendered(): void
+    // {
+    //     $response = $this->get('/login');
+
+    //     $response->assertStatus(200);
+    // }
     public function test_login_screen_can_be_rendered(): void
     {
+        $this->withoutExceptionHandling(); // <- ligne ajoutée
+
         $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
+
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
